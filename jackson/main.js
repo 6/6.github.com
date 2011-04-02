8d159e9adc5dc5d4bdee431b9aad037cf728a1cd
Peter Graham <pg.sners@gmail.com>
2011-04-02 00:30:16 -0400

    updated boxes

--- jackson/main.js
+++ jackson/main.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
     $("#add-new-list").click(addNewList);
+    $("#change-size").submit(changeSize);
+    changeSize();
 });
 
-function doAjaxPost(postLocation, vars, toTempDisable, successMessage) {
-        $(toTempDisable)
-    for(var i=0; i<toTempDisable.length; i++) {
-        $(toTempDisable[i]).attr('disabled','true');
-    }
+// do an ajax post to given location with given vars. Return bool indicating 
+// whether or not successful.
+function doAjaxPost(postLocation, vars) {
+    var success = false;
     $.post(postLocation, vars).success(function() {
-        alert(successMessage);
+        success = true;
     }).error(function(){
         alert("Unable to contact server.");
     });
-    for(var i=0; i<toTempDisable.length; i++) {
-        $(toTempDisable[i]).removeAttr('disabled');
-    }
+    return error;
 }
 
 // AJAX call to add a new list
@@ -24,6 +25,24 @@ function addNewList(){
         alert("Please enter a list name");
         return !1;
     }
-    doAjaxPost('/replace',{'list-name':name},['#list-name'],"Hooray!");
+    doAjaxPost('/replace',{'list-name':name});
+    $("#lists").append('<li><b><a href="#list">'+name+'</a></b> <a href="#remove">Remove</a>');
+    return !1;
+}
+
+// set the width of sortable boxes
+function setSortableWidth(numColumns) {
+    var width = numColumns*(2+10+10+70);
+    $("#sortable").css("width",width+"px");
+}
+
+function changeSize() {
+    var cols = parseInt($("#cols").val());
+    var rows = parseInt($("#rows").val());
+    setSortableWidth(cols);
+    $("#sortable").html("");
+    for(var i=0;i<(rows*cols);i++) {
+        $("#sortable").append('<li class="ui-state-default">'+(i+1)+'</li>');
+    }
     return !1;
 }
